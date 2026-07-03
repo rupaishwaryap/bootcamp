d5f9c9058a9e1ecbfbed2313fed5b29bc0614520
rupaishwaryap <24wh1a05g8@bvrithyderabad.edu.in>
2026-07-03 16:03:31 +0530

    Final

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,31 @@
-console.log("Hello from JS");
-/*alert("Hello from JavaScript!");*/
+const users = [
+  {
+    name: "Jane Doe",
+    gender: "Female",
+    image: "./images/jane.png"
+  },
+  {
+    name: "John Doe",
+    gender: "Male",
+    image: "./images/john.png"
+  }
+];
+
+let curUserId = 0;
+
+function toggle() {
+
+    if (curUserId == 0)
+        curUserId = 1;
+    else
+        curUserId = 0;
+
+    // Update Image
+    document.getElementById("user-img").src = users[curUserId].image;
+
+    // Update Name
+    document.getElementById("card-name").innerText = users[curUserId].name;
+
+    // Update Gender
+    document.getElementById("card-gender").innerText = users[curUserId].gender;
+}
