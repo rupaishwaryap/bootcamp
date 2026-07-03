3d61c05535f557f8f33a86d5d4c197291ed2ca47
rupaishwaryap <24wh1a05g8@bvrithyderabad.edu.in>
2026-07-03 12:01:43 +0530

    Initial Express server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3002;
+
+//webserver
+app.use(express.static("frontend"));
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
